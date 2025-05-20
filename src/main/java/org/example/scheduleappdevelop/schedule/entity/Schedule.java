ec450f4af9fa5de2c19573840a82dad589547241
seoyeon <dlatjdus0303@naver.com>
2025-05-20 14:03:24 +0900

    feat: apply validation-based exception handling

--- src/main/java/org/example/scheduleappdevelop/schedule/entity/Schedule.java
+++ src/main/java/org/example/scheduleappdevelop/schedule/entity/Schedule.java
@@ -1,10 +1,11 @@
 package org.example.scheduleappdevelop.schedule.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import org.example.scheduleappdevelop.common.entity.BaseEntity;
 import org.example.scheduleappdevelop.user.entity.User;
-
 
 @Getter
 @Entity
@@ -15,9 +16,13 @@ public class Schedule extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "제목은 필수값입니다.")
+    @Size(min = 1, max = 15, message = "제목은 1자 이상, 15자 이하로 적어주세요.")
     @Column(nullable = false)
     private String title;
 
+    @NotBlank(message = "내용은 필수값입니다.")
+    @Size(min = 1, max = 50, message = "내용은 1자 이상, 50자 이하로 적어주세요.")
     @Column(nullable = false)
     private String contents;
 
--- src/main/java/org/example/scheduleappdevelop/user/entity/User.java
+++ src/main/java/org/example/scheduleappdevelop/user/entity/User.java
@@ -1,6 +1,9 @@
 package org.example.scheduleappdevelop.user.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import org.example.scheduleappdevelop.common.entity.BaseEntity;
 
@@ -13,11 +16,17 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "이름은 필수값입니다.")
     @Column(nullable = false)
     private String username;
 
+    @Email
+    @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$")
+    @NotBlank(message = "이메일은 필수값입니다.")
+    @Column(nullable = false)
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수값입니다.")
     @Column(nullable = false)
     private String password;
 
