767b075ffc0ab8d9db8dad67a87869d5148e5d38
seoyeon <dlatjdus0303@naver.com>
2025-05-26 13:40:08 +0900

    fix: apply password encryption on update

--- src/main/java/org/example/scheduleappdevelop/user/service/UserService.java
+++ src/main/java/org/example/scheduleappdevelop/user/service/UserService.java
@@ -56,12 +56,14 @@ public class UserService {
 
         User findUser = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."));
 
-        if (!findUser.getPassword().equals(oldPassword)) {
+        if (!passwordEncoder.matches(oldPassword, findUser.getPassword())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
 
         }
 
-        findUser.updatePassword(newPassword);
+        String updateNewPassword = passwordEncoder.encode(newPassword);
+
+        findUser.updatePassword(updateNewPassword);
     }
 
     public void delete(Long id) {
